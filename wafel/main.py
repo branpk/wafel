74e61dd073662d943486bdc1b774f364f53cb2c9
Brandon Pickering <brandonpickering95@gmail.com>
2020-03-01 16:36:36 -0800

    Move window to new file

--- wafel/main.py
+++ wafel/main.py
@@ -24,6 +24,7 @@ from wafel.variable_format import Formatters
 from wafel.format_m64 import load_m64, save_m64
 from wafel.format_wafi import load_wafi, save_wafi
 from wafel.tas_metadata import TasMetadata
+from wafel.window import open_window_and_run
 
 
 DEFAULT_FRAME_SHEET_VARS = [
@@ -211,15 +212,28 @@ class View:
       ig.end_menu_bar()
 
 
-  def render(self, window_size: Tuple[int, int]) -> None:
-    ig.set_next_window_position(0, 0)
-    ig.set_next_window_size(*window_size)
-    ig.begin(
-      'Main##' + str(self.epoch),
-      False,
-      ig.WINDOW_NO_SAVED_SETTINGS | ig.WINDOW_NO_RESIZE | ig.WINDOW_NO_TITLE_BAR | ig.WINDOW_MENU_BAR,
-    )
+  def render(self) -> None:
+    # TODO: Move keyboard handling somewhere else
+    # TODO: Make this work when holding down mouse button
+    model = self.model
+    ig.get_io().key_repeat_rate = 1/30
+    if not ig.get_io().want_capture_keyboard:
+      if ig.is_key_pressed(ig.get_key_index(ig.KEY_DOWN_ARROW)) or \
+          ig.is_key_pressed(ig.get_key_index(ig.KEY_RIGHT_ARROW)):
+        model.selected_frame += 1
+      if ig.is_key_pressed(ig.get_key_index(ig.KEY_UP_ARROW)) or \
+          ig.is_key_pressed(ig.get_key_index(ig.KEY_LEFT_ARROW)):
+        model.selected_frame -= 1
+      if ig.is_key_pressed(ig.get_key_index(ig.KEY_PAGE_DOWN)):
+        model.selected_frame += 5
+      if ig.is_key_pressed(ig.get_key_index(ig.KEY_PAGE_UP)):
+        model.selected_frame -= 5
 
+
+    ig.push_id(str(self.epoch))
+
+    ig_window_size = ig.get_window_size()
+    window_size = (int(ig_window_size.x), int(ig_window_size.y))
     self.render_menu_bar()
 
     ig.columns(2)
@@ -228,7 +242,7 @@ class View:
     self.render_right_column()
     ig.columns(1)
 
-    ig.end()
+    ig.pop_id()
 
 
   def dbg_is_key_pressed(self, key: int) -> bool:
@@ -245,90 +259,20 @@ class View:
       return False
 
 
-def render(window, ig_renderer, view: View) -> None:
-  # TODO: Move keyboard handling somewhere else
-  # TODO: Make this work when holding down mouse button
-  model = view.model
-  ig.get_io().key_repeat_rate = 1/30
-  if not ig.get_io().want_capture_keyboard:
-    if ig.is_key_pressed(ig.get_key_index(ig.KEY_DOWN_ARROW)) or \
-        ig.is_key_pressed(ig.get_key_index(ig.KEY_RIGHT_ARROW)):
-      model.selected_frame += 1
-    if ig.is_key_pressed(ig.get_key_index(ig.KEY_UP_ARROW)) or \
-        ig.is_key_pressed(ig.get_key_index(ig.KEY_LEFT_ARROW)):
-      model.selected_frame -= 1
-    if ig.is_key_pressed(ig.get_key_index(ig.KEY_PAGE_DOWN)):
-      model.selected_frame += 5
-    if ig.is_key_pressed(ig.get_key_index(ig.KEY_PAGE_UP)):
-      model.selected_frame -= 5
-
-    if view.dbg_is_key_pressed(ord(']')):
-      view.dbg_frame_advance = not view.dbg_frame_advance
-
-  if view.dbg_frame_advance and not view.dbg_is_key_pressed(ord('\\')):
-    glfw.swap_buffers(window)
-    return
-
-  style = ig.get_style()
-  style.window_rounding = 0
-
-  window_size = glfw.get_window_size(window)
-
-  gl.glScissor(0, 0, *window_size)
-  gl.glClearColor(0.06, 0.06, 0.06, 1.0)
-  gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-
-  ig.get_style().colors[ig.COLOR_WINDOW_BACKGROUND] = (0, 0, 0, 0)
-  ig.new_frame()
-  view.render(window_size)
-  ig.end_frame()
-  ig.render()
-
-  draw_data = ig.get_draw_data()
-  ig_renderer.render(draw_data)
-
-  glfw.swap_buffers(window)
-
-
 def run() -> None:
-  glfw.init()
-
-  glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-  glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-  glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_COMPAT_PROFILE) # TODO: Core
-  glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, True)
-  glfw.window_hint(glfw.SAMPLES, 4)
-
-  glfw.window_hint(glfw.VISIBLE, False)
-  window = glfw.create_window(800, 600, 'Wafel', None, None)
-  glfw.set_window_size_limits(window, 1, 1, glfw.DONT_CARE, glfw.DONT_CARE)
-  glfw.maximize_window(window)
-  glfw.show_window(window)
-
-  glfw.make_context_current(window)
-
-  ig_context = ig.create_context()
-  ig_renderer = GlfwRenderer(window)
-  ig_renderer.io.ini_filename = None
-
-  def refresh_callback(window):
-    render(window, ig_renderer, view)
-  glfw.set_window_refresh_callback(window, refresh_callback)
-
   model = Model()
-  view = View(model)
-  view.file = SequenceFile('test_files/1key_j.m64', 'm64')
-  view.reload()
-  view.file = None
+  view = None
 
-  while not glfw.window_should_close(window):
-    glfw.poll_events()
-    ig_renderer.process_inputs()
-    render(window, ig_renderer, view)
+  # TODO: Clean up (use local_state)
+  def render():
+    nonlocal view
+    if view is None:
+      view = View(model)
+      view.file = SequenceFile('test_files/1key_j.m64', 'm64')
+      view.reload()
+      view.file = None
+    view.render()
+
     model.timeline.balance_distribution(1/120)
 
-  ig_renderer.shutdown()
-  ig.destroy_context(ig_context)
-
-  glfw.destroy_window(window)
-  glfw.terminate()
+  open_window_and_run(render)
---
+++ wafel/window.py
@@ -0,0 +1,82 @@
+from typing import *
+
+import glfw
+import imgui as ig
+from imgui.integrations.glfw import GlfwRenderer
+from OpenGL import GL as gl
+
+
+def _render_window(window, ig_renderer, render: Callable[[], None]) -> None:
+  #   if view.dbg_is_key_pressed(ord(']')):
+  #     view.dbg_frame_advance = not view.dbg_frame_advance
+
+  # if view.dbg_frame_advance and not view.dbg_is_key_pressed(ord('\\')):
+  #   glfw.swap_buffers(window)
+  #   return
+
+  style = ig.get_style()
+  style.window_rounding = 0
+
+  window_size = glfw.get_window_size(window)
+
+  gl.glScissor(0, 0, *window_size)
+  gl.glClearColor(0.06, 0.06, 0.06, 1.0)
+  gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+  ig.get_style().colors[ig.COLOR_WINDOW_BACKGROUND] = (0, 0, 0, 0)
+  ig.new_frame()
+
+  ig.set_next_window_position(0, 0)
+  ig.set_next_window_size(*window_size)
+  ig.begin(
+    'Main',
+    False,
+    ig.WINDOW_NO_SAVED_SETTINGS | ig.WINDOW_NO_RESIZE | ig.WINDOW_NO_TITLE_BAR | ig.WINDOW_MENU_BAR,
+  )
+  render()
+  ig.end()
+
+  ig.end_frame()
+  ig.render()
+
+  draw_data = ig.get_draw_data()
+  ig_renderer.render(draw_data)
+
+  glfw.swap_buffers(window)
+
+
+def open_window_and_run(render: Callable[[], None]) -> None:
+  glfw.init()
+
+  glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+  glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+  glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_COMPAT_PROFILE) # TODO: Core
+  glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, True)
+  glfw.window_hint(glfw.SAMPLES, 4)
+
+  glfw.window_hint(glfw.VISIBLE, False)
+  window = glfw.create_window(800, 600, 'Wafel', None, None)
+  glfw.set_window_size_limits(window, 1, 1, glfw.DONT_CARE, glfw.DONT_CARE)
+  glfw.maximize_window(window)
+  glfw.show_window(window)
+
+  glfw.make_context_current(window)
+
+  ig_context = ig.create_context()
+  ig_renderer = GlfwRenderer(window)
+  ig_renderer.io.ini_filename = None
+
+  def refresh_callback(window):
+    _render_window(window, ig_renderer, render)
+  glfw.set_window_refresh_callback(window, refresh_callback)
+
+  while not glfw.window_should_close(window):
+    glfw.poll_events()
+    ig_renderer.process_inputs()
+    _render_window(window, ig_renderer, render)
+
+  ig_renderer.shutdown()
+  ig.destroy_context(ig_context)
+
+  glfw.destroy_window(window)
+  glfw.terminate()
