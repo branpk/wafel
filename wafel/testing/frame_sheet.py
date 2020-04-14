7c551d0a1364f1108e36d037b4baa62065db18ce
Brandon Pickering <brandonpickering95@gmail.com>
2020-04-13 23:13:47 -0700

    Update frame sheet test

--- wafel/testing/frame_sheet.py
+++ wafel/testing/frame_sheet.py
@@ -5,23 +5,23 @@ from dataclasses import dataclass
 import time
 
 import wafel.imgui as ig
-from wafel.variable import VariableAccessor, Variable
+from wafel.variable import VariableReader, VariableWriter, Variable, VariablePipeline
 from wafel.variable_display import VariableDisplayer
 from wafel.variable_format import Formatters, DecimalIntFormatter, VariableFormatter
 from wafel.local_state import use_state_with, use_state
 from wafel.frame_sheet import FrameSequence, FrameSheet, CellDragHandler
 from wafel.util import *
-from wafel.range_edit import RangeEditAccessor
+from wafel.range_edit import RangeEditWriter
 
 
-class Accessor(VariableAccessor):
+class Accessor(VariableReader, VariableWriter):
   def __init__(self) -> None:
     self.edits: Dict[Variable, object] = {}
 
-  def get(self, variable: Variable) -> object:
+  def read(self, variable: Variable) -> object:
     return self.edits.get(variable, 0)
 
-  def set(self, variable: Variable, value: object) -> None:
+  def write(self, variable: Variable, value: object) -> None:
     self.edits[variable] = value
 
   def reset(self, variable: Variable) -> None:
@@ -33,13 +33,15 @@ class Model(VariableDisplayer, Formatters, FrameSequence):
   def __init__(self) -> None:
     self._selected_frame = 0
     self._max_frame = 1000
-    self._accessor = RangeEditAccessor(Accessor())
+    accessor = Accessor()
+    self._range_edit_writer = RangeEditWriter(accessor)
+    self._pipeline = VariablePipeline(self._range_edit_writer, accessor)
     for var in ['stick', 'vel', 'pos']:
       for f in range(100):
-        self._accessor.set(Variable(var, frame=f), 10 + f)
+        self._pipeline.write(Variable(var, frame=f), 10 + f)
     for i in range(10):
       for f in range(100):
-        self._accessor.set(Variable('junk ' + str(i), frame=f), 10 + f)
+        self._pipeline.write(Variable('junk ' + str(i), frame=f), 10 + f)
 
   def label(self, variable: Variable) -> str:
     return variable.name
@@ -79,7 +81,7 @@ def test_frame_sheet(id: str) -> None:
   model = use_state_with('model', lambda: Model()).value
 
   def make_sheet() -> FrameSheet:
-    sheet = FrameSheet(model, model._accessor, model._accessor, model, model)
+    sheet = FrameSheet(model, model._pipeline, model._range_edit_writer, model, model)
     sheet.append_variable(Variable('stick'))
     sheet.append_variable(Variable('vel'))
     sheet.append_variable(Variable('pos'))
