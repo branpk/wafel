d4b52984d4ff3ca84efd1b130265c467a773d175
Brandon Pickering <brandonpickering95@gmail.com>
2020-08-27 09:39:09 -0700

    Input

--- ext_modules/core/src/python/mod.rs
+++ ext_modules/core/src/python/mod.rs
@@ -5,12 +5,19 @@
 use bytemuck::{cast_slice, Pod, Zeroable};
 pub use pipeline::*;
 use pyo3::prelude::*;
-use std::{iter, slice};
+use std::{
+    collections::{HashMap, HashSet},
+    iter, slice,
+    time::Instant,
+};
 pub use variable::*;
 use wgpu::util::DeviceExt;
 use winit::{
     dpi::PhysicalSize,
-    event::{Event, WindowEvent},
+    event::{
+        ElementState::{Pressed, Released},
+        Event, KeyboardInput, MouseButton, MouseScrollDelta, VirtualKeyCode, WindowEvent,
+    },
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
@@ -208,9 +215,6 @@ impl PyRenderer {
                 alpha_to_coverage_enabled: false,
             });
 
-            // io.setattr("delta_time", 1.0 / 60.0)?;
-            io.setattr("display_size", (800, 600))?;
-
             let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
                 label: None,
                 address_mode_u: wgpu::AddressMode::ClampToEdge,
@@ -285,28 +289,117 @@ impl PyRenderer {
             io.getattr("fonts")?.setattr("texture_id", 0)?;
             io.getattr("fonts")?.call_method0("clear_tex_data")?;
 
-            event_loop.run(move |event, _, control_flow| {
-                match event {
-                    Event::WindowEvent { event, .. } => match event {
-                        WindowEvent::Resized(size) => {
-                            swap_chain_desc.width = size.width;
-                            swap_chain_desc.height = size.height;
-                            swap_chain = device.create_swap_chain(&surface, &swap_chain_desc);
-                        }
-                        WindowEvent::CloseRequested => {
-                            *control_flow = ControlFlow::Exit;
-                        }
-                        _ => {}
-                    },
-                    Event::MainEventsCleared => window.request_redraw(),
-                    Event::RedrawRequested(_) => {
-                        if swap_chain_desc.width > 0 && swap_chain_desc.height > 0 {
-                            let gil = Python::acquire_gil();
-                            let py = gil.python();
+            let key_map: Vec<(&str, &str)> = vec![
+                ("KEY_TAB", "KEY_TAB"),
+                ("KEY_LEFT_ARROW", "KEY_LEFT"),
+                ("KEY_RIGHT_ARROW", "KEY_RIGHT"),
+                ("KEY_UP_ARROW", "KEY_UP"),
+                ("KEY_DOWN_ARROW", "KEY_DOWN"),
+                ("KEY_PAGE_UP", "KEY_PAGE_UP"),
+                ("KEY_PAGE_DOWN", "KEY_PAGE_DOWN"),
+                ("KEY_HOME", "KEY_HOME"),
+                ("KEY_END", "KEY_END"),
+                ("KEY_DELETE", "KEY_DELETE"),
+                ("KEY_BACKSPACE", "KEY_BACKSPACE"),
+                ("KEY_ENTER", "KEY_ENTER"),
+                ("KEY_ESCAPE", "KEY_ESCAPE"),
+                ("KEY_A", "KEY_A"),
+                ("KEY_C", "KEY_C"),
+                ("KEY_V", "KEY_V"),
+                ("KEY_X", "KEY_X"),
+                ("KEY_Y", "KEY_Y"),
+                ("KEY_Z", "KEY_Z"),
+            ];
+            let glfw = PyModule::import(py, "glfw")?;
+            for (imgui_name, glfw_name) in key_map {
+                let imgui_key = ig.getattr(imgui_name)?;
+                let glfw_key = glfw.getattr(glfw_name)?;
+                io.getattr("key_map")?.set_item(imgui_key, glfw_key)?;
+            }
 
-                            let result: PyResult<()> = try {
-                                let ig = PyModule::import(py, "imgui")?;
-                                let io = ig.call_method0("get_io")?;
+            let mut winit_to_glfw_key: HashMap<VirtualKeyCode, u32> = HashMap::new();
+            for (glfw_name, winit_key) in GLFW_WINIT_KEY_MAP {
+                let glfw_key: u32 = glfw.getattr(glfw_name)?.extract()?;
+                winit_to_glfw_key.insert(*winit_key, glfw_key);
+            }
+
+            let mut last_frame_time = Instant::now();
+
+            event_loop.run(move |event, _, control_flow| {
+                let gil = Python::acquire_gil();
+                let py = gil.python();
+
+                let result: PyResult<()> = try {
+                    let ig = PyModule::import(py, "imgui")?;
+                    let io = ig.call_method0("get_io")?;
+
+                    match event {
+                        Event::WindowEvent { event, .. } => match event {
+                            WindowEvent::Resized(size) => {
+                                swap_chain_desc.width = size.width;
+                                swap_chain_desc.height = size.height;
+                                swap_chain = device.create_swap_chain(&surface, &swap_chain_desc);
+                            }
+                            WindowEvent::CloseRequested => {
+                                *control_flow = ControlFlow::Exit;
+                            }
+                            WindowEvent::CursorMoved { position, .. } => {
+                                io.setattr("mouse_pos", (position.x, position.y))?;
+                            }
+                            WindowEvent::MouseInput { state, button, .. } => {
+                                let is_down = match state {
+                                    Pressed => true,
+                                    Released => false,
+                                };
+                                let button_index = match button {
+                                    MouseButton::Left => Some(0),
+                                    MouseButton::Right => Some(1),
+                                    MouseButton::Middle => Some(2),
+                                    MouseButton::Other(_) => None,
+                                };
+                                if let Some(button_index) = button_index {
+                                    io.getattr("mouse_down")?.set_item(button_index, is_down)?;
+                                }
+                            }
+                            WindowEvent::MouseWheel { delta, .. } => {
+                                if let MouseScrollDelta::LineDelta(_, y) = delta {
+                                    let mouse_wheel: f32 = io.getattr("mouse_wheel")?.extract()?;
+                                    io.setattr("mouse_wheel", mouse_wheel + y)?;
+                                }
+                            }
+                            WindowEvent::KeyboardInput {
+                                input:
+                                    KeyboardInput {
+                                        state,
+                                        virtual_keycode,
+                                        ..
+                                    },
+                                ..
+                            } => {
+                                let is_down = match state {
+                                    Pressed => true,
+                                    Released => false,
+                                };
+                                if let Some(winit_key) = virtual_keycode {
+                                    if let Some(&glfw_key) = winit_to_glfw_key.get(&winit_key) {
+                                        io.getattr("keys_down")?.set_item(glfw_key, is_down)?;
+                                    }
+                                }
+                            }
+                            WindowEvent::ReceivedCharacter(c) => {
+                                let c = c as u32;
+                                if c < 0x10000 {
+                                    io.call_method1("add_input_character", (c,))?;
+                                }
+                            }
+                            _ => {}
+                        },
+                        Event::MainEventsCleared => window.request_redraw(),
+                        Event::RedrawRequested(_) => {
+                            if swap_chain_desc.width > 0 && swap_chain_desc.height > 0 {
+                                let delta_time = last_frame_time.elapsed().as_secs_f64();
+                                last_frame_time = Instant::now();
+                                io.setattr("delta_time", delta_time)?;
 
                                 let display_size = (swap_chain_desc.width, swap_chain_desc.height);
                                 io.setattr("display_size", display_size)?;
@@ -464,14 +557,14 @@ impl PyRenderer {
 
                                 let command_buffer = encoder.finish();
                                 queue.submit(iter::once(command_buffer));
-                            };
-                            if let Err(error) = result {
-                                error.print(py);
-                                *control_flow = ControlFlow::Exit;
                             }
                         }
+                        _ => {}
                     }
-                    _ => {}
+                };
+                if let Err(error) = result {
+                    error.print(py);
+                    *control_flow = ControlFlow::Exit;
                 }
             });
         })
@@ -489,3 +582,126 @@ struct Vertex {
 
 unsafe impl Zeroable for Vertex {}
 unsafe impl Pod for Vertex {}
+
+// TODO: Stop using GLFW for input. Will have to define a mapping from VirtualKeyCode
+// to int though
+const GLFW_WINIT_KEY_MAP: &[(&'static str, VirtualKeyCode)] = &[
+    ("KEY_SPACE", VirtualKeyCode::Space),
+    ("KEY_APOSTROPHE", VirtualKeyCode::Apostrophe),
+    ("KEY_COMMA", VirtualKeyCode::Comma),
+    ("KEY_MINUS", VirtualKeyCode::Minus),
+    ("KEY_PERIOD", VirtualKeyCode::Period),
+    ("KEY_SLASH", VirtualKeyCode::Slash),
+    ("KEY_0", VirtualKeyCode::Key0),
+    ("KEY_1", VirtualKeyCode::Key1),
+    ("KEY_2", VirtualKeyCode::Key2),
+    ("KEY_3", VirtualKeyCode::Key3),
+    ("KEY_4", VirtualKeyCode::Key4),
+    ("KEY_5", VirtualKeyCode::Key5),
+    ("KEY_6", VirtualKeyCode::Key6),
+    ("KEY_7", VirtualKeyCode::Key7),
+    ("KEY_8", VirtualKeyCode::Key8),
+    ("KEY_9", VirtualKeyCode::Key9),
+    ("KEY_SEMICOLON", VirtualKeyCode::Semicolon),
+    ("KEY_EQUAL", VirtualKeyCode::Equals),
+    ("KEY_A", VirtualKeyCode::A),
+    ("KEY_B", VirtualKeyCode::B),
+    ("KEY_C", VirtualKeyCode::C),
+    ("KEY_D", VirtualKeyCode::D),
+    ("KEY_E", VirtualKeyCode::E),
+    ("KEY_F", VirtualKeyCode::F),
+    ("KEY_G", VirtualKeyCode::G),
+    ("KEY_H", VirtualKeyCode::H),
+    ("KEY_I", VirtualKeyCode::I),
+    ("KEY_J", VirtualKeyCode::J),
+    ("KEY_K", VirtualKeyCode::K),
+    ("KEY_L", VirtualKeyCode::L),
+    ("KEY_M", VirtualKeyCode::M),
+    ("KEY_N", VirtualKeyCode::N),
+    ("KEY_O", VirtualKeyCode::O),
+    ("KEY_P", VirtualKeyCode::P),
+    ("KEY_Q", VirtualKeyCode::Q),
+    ("KEY_R", VirtualKeyCode::R),
+    ("KEY_S", VirtualKeyCode::S),
+    ("KEY_T", VirtualKeyCode::T),
+    ("KEY_U", VirtualKeyCode::U),
+    ("KEY_V", VirtualKeyCode::V),
+    ("KEY_W", VirtualKeyCode::W),
+    ("KEY_X", VirtualKeyCode::X),
+    ("KEY_Y", VirtualKeyCode::Y),
+    ("KEY_Z", VirtualKeyCode::Z),
+    ("KEY_LEFT_BRACKET", VirtualKeyCode::LBracket),
+    ("KEY_BACKSLASH", VirtualKeyCode::Backslash),
+    ("KEY_RIGHT_BRACKET", VirtualKeyCode::RBracket),
+    ("KEY_GRAVE_ACCENT", VirtualKeyCode::Grave),
+    ("KEY_ESCAPE", VirtualKeyCode::Escape),
+    ("KEY_ENTER", VirtualKeyCode::Return),
+    ("KEY_TAB", VirtualKeyCode::Tab),
+    ("KEY_BACKSPACE", VirtualKeyCode::Back),
+    ("KEY_INSERT", VirtualKeyCode::Insert),
+    ("KEY_DELETE", VirtualKeyCode::Delete),
+    ("KEY_RIGHT", VirtualKeyCode::Right),
+    ("KEY_LEFT", VirtualKeyCode::Left),
+    ("KEY_DOWN", VirtualKeyCode::Down),
+    ("KEY_UP", VirtualKeyCode::Up),
+    ("KEY_PAGE_UP", VirtualKeyCode::PageUp),
+    ("KEY_PAGE_DOWN", VirtualKeyCode::PageDown),
+    ("KEY_HOME", VirtualKeyCode::Home),
+    ("KEY_END", VirtualKeyCode::End),
+    ("KEY_CAPS_LOCK", VirtualKeyCode::Capital),
+    ("KEY_SCROLL_LOCK", VirtualKeyCode::Scroll),
+    ("KEY_NUM_LOCK", VirtualKeyCode::Numlock),
+    // ("KEY_PRINT_SCREEN", VirtualKeyCode::PRINT_SCREEN),
+    ("KEY_PAUSE", VirtualKeyCode::Pause),
+    ("KEY_F1", VirtualKeyCode::F1),
+    ("KEY_F2", VirtualKeyCode::F2),
+    ("KEY_F3", VirtualKeyCode::F3),
+    ("KEY_F4", VirtualKeyCode::F4),
+    ("KEY_F5", VirtualKeyCode::F5),
+    ("KEY_F6", VirtualKeyCode::F6),
+    ("KEY_F7", VirtualKeyCode::F7),
+    ("KEY_F8", VirtualKeyCode::F8),
+    ("KEY_F9", VirtualKeyCode::F9),
+    ("KEY_F10", VirtualKeyCode::F10),
+    ("KEY_F11", VirtualKeyCode::F11),
+    ("KEY_F12", VirtualKeyCode::F12),
+    ("KEY_F13", VirtualKeyCode::F13),
+    ("KEY_F14", VirtualKeyCode::F14),
+    ("KEY_F15", VirtualKeyCode::F15),
+    ("KEY_F16", VirtualKeyCode::F16),
+    ("KEY_F17", VirtualKeyCode::F17),
+    ("KEY_F18", VirtualKeyCode::F18),
+    ("KEY_F19", VirtualKeyCode::F19),
+    ("KEY_F20", VirtualKeyCode::F20),
+    ("KEY_F21", VirtualKeyCode::F21),
+    ("KEY_F22", VirtualKeyCode::F22),
+    ("KEY_F23", VirtualKeyCode::F23),
+    ("KEY_F24", VirtualKeyCode::F24),
+    // ("KEY_F25", VirtualKeyCode::F25),
+    ("KEY_KP_0", VirtualKeyCode::Numpad0),
+    ("KEY_KP_1", VirtualKeyCode::Numpad1),
+    ("KEY_KP_2", VirtualKeyCode::Numpad2),
+    ("KEY_KP_3", VirtualKeyCode::Numpad3),
+    ("KEY_KP_4", VirtualKeyCode::Numpad4),
+    ("KEY_KP_5", VirtualKeyCode::Numpad5),
+    ("KEY_KP_6", VirtualKeyCode::Numpad6),
+    ("KEY_KP_7", VirtualKeyCode::Numpad7),
+    ("KEY_KP_8", VirtualKeyCode::Numpad8),
+    ("KEY_KP_9", VirtualKeyCode::Numpad9),
+    ("KEY_KP_DECIMAL", VirtualKeyCode::Decimal),
+    ("KEY_KP_DIVIDE", VirtualKeyCode::Divide),
+    ("KEY_KP_MULTIPLY", VirtualKeyCode::Multiply),
+    ("KEY_KP_SUBTRACT", VirtualKeyCode::Subtract),
+    ("KEY_KP_ADD", VirtualKeyCode::Add),
+    ("KEY_KP_ENTER", VirtualKeyCode::NumpadEnter),
+    ("KEY_KP_EQUAL", VirtualKeyCode::NumpadEquals),
+    ("KEY_LEFT_SHIFT", VirtualKeyCode::LShift),
+    ("KEY_LEFT_CONTROL", VirtualKeyCode::LControl),
+    ("KEY_LEFT_ALT", VirtualKeyCode::LAlt),
+    ("KEY_LEFT_SUPER", VirtualKeyCode::LWin), // ?
+    ("KEY_RIGHT_SHIFT", VirtualKeyCode::RShift),
+    ("KEY_RIGHT_CONTROL", VirtualKeyCode::RControl),
+    ("KEY_RIGHT_ALT", VirtualKeyCode::RAlt),
+    ("KEY_RIGHT_SUPER", VirtualKeyCode::RWin), // ?
+                                               // ("KEY_MENU", VirtualKeyCode::MENU),
+];
