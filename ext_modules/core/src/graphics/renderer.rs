49b23560c7edbff3c9bd97a37adcdd287ac84d6a
Brandon Pickering <brandonpickering95@gmail.com>
2020-08-30 06:17:36 -0700

    Enable multisample

--- ext_modules/core/src/graphics/renderer.rs
+++ ext_modules/core/src/graphics/renderer.rs
@@ -3,11 +3,11 @@ use crate::{
     python::{BirdsEyeCamera, Camera, RotateCamera, Scene, SurfaceType},
 };
 use bytemuck::{cast_slice, offset_of, Pod, Zeroable};
-use lazy_static::lazy_static;
 use nalgebra::distance;
 use std::{f32::consts::PI, iter, mem::size_of};
 use wgpu::util::DeviceExt;
 
+const NUM_OUTPUT_SAMPLES: u32 = 4;
 const DEPTH_TEXTURE_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Depth24Plus;
 
 #[derive(Debug, Clone, Copy, Default)]
@@ -37,6 +37,7 @@ struct SceneBundle {
 }
 
 pub struct Renderer {
+    multisample_texture: Option<((u32, u32), wgpu::Texture)>,
     depth_texture: Option<((u32, u32), wgpu::Texture)>,
     transform_bind_group_layout: wgpu::BindGroupLayout,
     surface_pipeline: wgpu::RenderPipeline,
@@ -106,6 +107,7 @@ impl Renderer {
         );
 
         Self {
+            multisample_texture: None,
             depth_texture: None,
             transform_bind_group_layout,
             surface_pipeline,
@@ -125,6 +127,24 @@ impl Renderer {
         output_format: wgpu::TextureFormat,
         scenes: &[Scene],
     ) {
+        if self
+            .multisample_texture
+            .as_ref()
+            .filter(|(size, _)| size == &output_size)
+            .is_none()
+        {
+            self.multisample_texture = Some((
+                output_size,
+                create_multisample_texture(device, output_format, output_size),
+            ));
+        }
+        let multisample_texture_view = self
+            .multisample_texture
+            .as_ref()
+            .unwrap()
+            .1
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
         if self
             .depth_texture
             .as_ref()
@@ -229,8 +249,8 @@ impl Renderer {
         {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
-                    attachment: output_view,
-                    resolve_target: None,
+                    attachment: &multisample_texture_view,
+                    resolve_target: Some(&output_view),
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color {
                             r: 0.06,
@@ -303,6 +323,26 @@ impl Renderer {
     }
 }
 
+fn create_multisample_texture(
+    device: &wgpu::Device,
+    output_format: wgpu::TextureFormat,
+    output_size: (u32, u32),
+) -> wgpu::Texture {
+    device.create_texture(&wgpu::TextureDescriptor {
+        label: None,
+        size: wgpu::Extent3d {
+            width: output_size.0,
+            height: output_size.1,
+            depth: 1,
+        },
+        mip_level_count: 1,
+        sample_count: NUM_OUTPUT_SAMPLES,
+        dimension: wgpu::TextureDimension::D2,
+        format: output_format,
+        usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+    })
+}
+
 fn create_depth_texture(device: &wgpu::Device, output_size: (u32, u32)) -> wgpu::Texture {
     device.create_texture(&wgpu::TextureDescriptor {
         label: None,
@@ -312,7 +352,7 @@ fn create_depth_texture(device: &wgpu::Device, output_size: (u32, u32)) -> wgpu:
             depth: 1,
         },
         mip_level_count: 1,
-        sample_count: 1,
+        sample_count: NUM_OUTPUT_SAMPLES,
         dimension: wgpu::TextureDimension::D2,
         format: DEPTH_TEXTURE_FORMAT,
         usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
@@ -383,7 +423,7 @@ fn create_surface_pipeline(
                 ],
             }],
         },
-        sample_count: 1,
+        sample_count: NUM_OUTPUT_SAMPLES,
         sample_mask: !0,
         alpha_to_coverage_enabled: false,
     })
@@ -458,7 +498,7 @@ fn create_wall_hitbox_pipeline(
                 ],
             }],
         },
-        sample_count: 1,
+        sample_count: NUM_OUTPUT_SAMPLES,
         sample_mask: !0,
         alpha_to_coverage_enabled: false,
     })
