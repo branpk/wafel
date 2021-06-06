71b3ad908ea8cf2d7947312fd2a755a5b8ebb2f9
Brandon Pickering <brandonpickering95@gmail.com>
2021-06-06 12:04:05 -0700

    Allow --stdout in libsm64_layout

--- libsm64_layout/src/main.rs
+++ libsm64_layout/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, process};
 
-use clap::{App, Arg};
+use clap::{App, Arg, ArgGroup};
 use wafel_layout::{load_layout_from_dll, load_sm64_extras};
 
 fn main() {
@@ -8,6 +8,7 @@ fn main() {
         .arg(
             Arg::with_name("input")
                 .short("i")
+                .long("input")
                 .value_name("FILE")
                 .help("path to sm64_xx.dll (NOT the .locked file)")
                 .required(true),
@@ -15,14 +16,24 @@ fn main() {
         .arg(
             Arg::with_name("output")
                 .short("o")
+                .long("output")
                 .value_name("FILE")
-                .help("path to the output JSON file")
-                .required(true),
+                .help("path to the output JSON file"),
+        )
+        .arg(
+            Arg::with_name("stdout")
+                .long("stdout")
+                .help("print the JSON text to stdout"),
+        )
+        .group(
+            ArgGroup::with_name("output-option")
+                .args(&["output", "stdout"])
+                .required(true)
+                .multiple(true),
         )
         .get_matches();
 
     let input_filename = matches.value_of("input").unwrap();
-    let output_filename = matches.value_of("output").unwrap();
 
     let mut layout = load_layout_from_dll(input_filename).unwrap_or_else(|error| {
         eprintln!("Error while parsing {}: {}", input_filename, error);
@@ -39,8 +50,13 @@ fn main() {
         process::exit(1);
     });
 
-    fs::write(output_filename, &layout_json).unwrap_or_else(|error| {
-        eprintln!("Error while writing {}: {}", output_filename, error);
-        process::exit(1);
-    });
+    if let Some(output_filename) = matches.value_of("output") {
+        fs::write(output_filename, &layout_json).unwrap_or_else(|error| {
+            eprintln!("Error while writing {}: {}", output_filename, error);
+            process::exit(1);
+        });
+    }
+    if matches.is_present("stdout") {
+        print!("{}", layout_json);
+    }
 }
