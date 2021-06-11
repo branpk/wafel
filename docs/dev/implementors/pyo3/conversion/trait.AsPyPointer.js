78783f03fb2c0c2d50ac3dfce9f37c2d8194c619
branpk <brandonpickering95@gmail.com>
2021-06-11 15:37:27 +0000

    Deploying to gh-pages from @ branpk/wafel@e0d58c59847a2f2961fdfd028b659a372490bc0f 🚀

---
+++ docs/dev/implementors/pyo3/conversion/trait.AsPyPointer.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["wafel"] = [{"text":"impl AsPyPointer for <a class=\"struct\" href=\"wafel/struct.WafelError.html\" title=\"struct wafel::WafelError\">WafelError</a>","synthetic":false,"types":["wafel::WafelError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
