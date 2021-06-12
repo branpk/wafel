0044e586712991bac943d2a4f80a403563ddd519
Brandon Pickering <brandonpickering95@gmail.com>
2021-06-11 17:10:08 -0700

    0.7.0

--- wafel/config.py
+++ wafel/config.py
@@ -9,7 +9,7 @@ from wafel.util import assert_not_none, log
 
 
 # Important: be sure to re-run `python build.py dist lock` after changing!
-version = (0, 6, 4)
+version = (0, 7, 0)
 
 dev_mode: bool
 assets_directory: str
