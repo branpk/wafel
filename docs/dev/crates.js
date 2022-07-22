6f4a103d202b80e52251f817709f0ef5fb676b27
branpk <brandonpickering95@gmail.com>
2022-07-22 21:59:10 +0000

    Deploying to gh-pages from @ branpk/wafel@f4c3e3dad5600d609f69dd76185a896f0f2217e0 🚀

--- docs/dev/crates.js
+++ docs/dev/crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["fast3d","libsm64_lock","sm64_layout","sm64_var_dump","wafel","wafel_api","wafel_core","wafel_data_access","wafel_data_access_derive","wafel_data_type","wafel_layout","wafel_memory","wafel_timeline","wafel_viz","wafel_viz_tests"];
+window.ALL_CRATES = ["fast3d","libsm64_lock","sm64_layout","sm64_var_dump","wafel","wafel_api","wafel_core","wafel_data_access","wafel_data_access_derive","wafel_data_type","wafel_layout","wafel_memory","wafel_sm64","wafel_timeline","wafel_viz","wafel_viz_tests"];
