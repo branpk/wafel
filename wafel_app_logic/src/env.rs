9f06ca7b9e00a83c0510b39cbb43998239a74995
Brandon Pickering <brandonpickering95@gmail.com>
2023-09-07 08:56:19 -0700

    Add documentation to wafel_app_logic

--- wafel_app_logic/src/env.rs
+++ wafel_app_logic/src/env.rs
@@ -1,5 +1,8 @@
-/// A trait defining the Wafel application's interaction with the external
-/// environment.
+/// A trait defining the Wafel application's interaction with the file system
+/// and windowing system.
+///
+/// The GUI implementation is in the wafel_app crate, but it can be overridden
+/// to run Wafel in headless mode for example.
 pub trait Env {
     /// Return the current version of Wafel.
     fn wafel_version(&self) -> &str;
--- wafel_app_logic/src/lib.rs
+++ wafel_app_logic/src/lib.rs
@@ -1,4 +1,9 @@
 //! The logic and UI for the main Wafel application.
+//!
+//! For testing purposes, this crate doesn't directly access the file system and
+//! is agnostic to the window/graphics backend.
+//! These operations are done indirectly through the [Env] trait, which can be
+//! overriden as needed.
 
 #![warn(missing_docs, missing_debug_implementations)]
 
