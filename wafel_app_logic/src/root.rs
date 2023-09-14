d3c2d02015a5d64adc938be2ca255f0ff279c4b3
Brandon Pickering <brandonpickering95@gmail.com>
2023-09-13 17:35:32 -0700

    Move error boundary to Wafel

--- wafel_app_logic/src/root.rs
+++ wafel_app_logic/src/root.rs
@@ -1,34 +1,6 @@
 use wafel_api::VizRenderData;
 
-use crate::{error_boundary::ErrorBoundary, workspace_root::WorkspaceRoot, Env};
-
-#[derive(Debug)]
-pub struct RootErrorBoundary {
-    error_boundary: ErrorBoundary,
-    root: Root,
-}
-
-impl RootErrorBoundary {
-    pub fn new() -> Self {
-        Self {
-            error_boundary: ErrorBoundary::new(),
-            root: Root::new(),
-        }
-    }
-
-    pub fn show(&mut self, env: &dyn Env, ctx: &egui::Context) -> Vec<VizRenderData> {
-        if self.error_boundary.has_error() {
-            egui::CentralPanel::default().show(ctx, |ui| {
-                self.error_boundary.show_error(env, ui);
-            });
-            Vec::new()
-        } else {
-            self.error_boundary
-                .catch_panic(env, || self.root.show(env, ctx))
-                .unwrap_or_default()
-        }
-    }
-}
+use crate::{workspace_root::WorkspaceRoot, Env};
 
 #[derive(Debug)]
 pub struct Root {
--- wafel_app_logic/src/wafel.rs
+++ wafel_app_logic/src/wafel.rs
@@ -1,17 +1,19 @@
 use wafel_api::VizRenderData;
 
-use crate::{root::RootErrorBoundary, Env};
+use crate::{error_boundary::ErrorBoundary, root::Root, Env};
 
 /// State of the Wafel application.
 #[derive(Debug)]
 pub struct Wafel {
-    root: RootErrorBoundary,
+    error_boundary: ErrorBoundary,
+    root: Root,
 }
 
 impl Default for Wafel {
     fn default() -> Self {
         Self {
-            root: RootErrorBoundary::new(),
+            error_boundary: ErrorBoundary::new(),
+            root: Root::new(),
         }
     }
 }
@@ -19,6 +21,15 @@ impl Default for Wafel {
 impl Wafel {
     /// Render the Wafel UI and respond to user input events.
     pub fn show(&mut self, env: &dyn Env, ctx: &egui::Context) -> Vec<VizRenderData> {
-        self.root.show(env, ctx)
+        if self.error_boundary.has_error() {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                self.error_boundary.show_error(env, ui);
+            });
+            Vec::new()
+        } else {
+            self.error_boundary
+                .catch_panic(env, || self.root.show(env, ctx))
+                .unwrap_or_default()
+        }
     }
 }
