041db63d354e3c59606d327ce726a3bd973a797a
branpk <brandonpickering95@gmail.com>
2023-10-07 16:48:51 +0000

    Deploying to gh-pages from @ branpk/wafel@abae7aa1eaa98461ed6ff023d44d0308cbc58ad4 🚀

--- docs/dev/wafel_viz/sidebar-items.js
+++ docs/dev/wafel_viz/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Element","TransparencyHint","Viewport"],"struct":["Camera","LineElement","Mat4","PointElement","Rect2","TriangleElement","Vec2","Vec3","Vec4","VizScene"]};
+window.SIDEBAR_ITEMS = {"enum":["Element","TransparencyHint","Viewport"],"struct":["Camera","LineElement","Mat4","PointElement","Rect2","Rect3","TriangleElement","Vec2","Vec3","Vec4","VizScene"]};
