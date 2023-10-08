f6950942cd01494c46e1b4c98db6dbc59007485f
branpk <brandonpickering95@gmail.com>
2023-10-08 00:34:31 +0000

    Deploying to gh-pages from @ branpk/wafel@67c6e5a969e33c0294bdd8b89216864ba7334f7c 🚀

--- docs/dev/implementors/pyo3/class/basic/trait.PyObjectRichcmpProtocol.js
+++ docs/dev/implementors/pyo3/class/basic/trait.PyObjectRichcmpProtocol.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"wafel":[["impl&lt;'p&gt; PyObjectRichcmpProtocol&lt;'p&gt; for <a class=\"struct\" href=\"wafel/struct.Input.html\" title=\"struct wafel::Input\">Input</a>"],["impl&lt;'p&gt; PyObjectRichcmpProtocol&lt;'p&gt; for <a class=\"struct\" href=\"wafel/struct.Address.html\" title=\"struct wafel::Address\">Address</a>"]],
+"wafel":[["impl&lt;'p&gt; PyObjectRichcmpProtocol&lt;'p&gt; for <a class=\"struct\" href=\"wafel/struct.Address.html\" title=\"struct wafel::Address\">Address</a>"],["impl&lt;'p&gt; PyObjectRichcmpProtocol&lt;'p&gt; for <a class=\"struct\" href=\"wafel/struct.Input.html\" title=\"struct wafel::Input\">Input</a>"]],
 "wafel_core":[["impl&lt;'p&gt; PyObjectRichcmpProtocol&lt;'p&gt; for <a class=\"struct\" href=\"wafel_core/python/struct.PyVariable.html\" title=\"struct wafel_core::python::PyVariable\">PyVariable</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
