67c6e5a969e33c0294bdd8b89216864ba7334f7c
Brandon Pickering <brandonpickering95@gmail.com>
2023-10-07 17:33:18 -0700

    Update egui and wgpu

--- wafel_app_ui/src/workspace.rs
+++ wafel_app_ui/src/workspace.rs
@@ -5,13 +5,13 @@ use crate::{data_explorer::DataExplorer, pane::Pane, Env};
 #[derive(Debug)]
 pub struct Workspace {
     emu: Emu,
-    tree: egui_dock::Tree<Pane>,
+    dock_state: egui_dock::DockState<Pane>,
 }
 
 impl Workspace {
     pub fn with_emu(emu: Emu) -> Self {
-        let mut tree = egui_dock::Tree::new(vec![Pane::DataExplorer(DataExplorer::new())]);
-        Self { emu, tree }
+        let dock_state = egui_dock::DockState::new(vec![Pane::DataExplorer(DataExplorer::new())]);
+        Self { emu, dock_state }
     }
 
     pub fn show(&mut self, env: &dyn Env, ui: &mut egui::Ui) -> Vec<VizScene> {
@@ -21,11 +21,10 @@ impl Workspace {
             emu: &mut self.emu,
             viz_render_data: &mut viz_render_data,
         };
-        egui_dock::DockArea::new(&mut self.tree)
+        egui_dock::DockArea::new(&mut self.dock_state)
             .style(egui_dock::Style::from_egui(ui.style()))
             .show_close_buttons(false)
             .tab_context_menus(false)
-            .scroll_area_in_tabs(false)
             .show_inside(ui, &mut tab_viewer);
         viz_render_data
     }
@@ -40,12 +39,16 @@ struct TabViewer<'a> {
 impl egui_dock::TabViewer for TabViewer<'_> {
     type Tab = Pane;
 
+    fn title(&mut self, tab: &mut Pane) -> egui::WidgetText {
+        tab.title().into()
+    }
+
+    fn scroll_bars(&self, _tab: &Self::Tab) -> [bool; 2] {
+        [false; 2]
+    }
+
     fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Pane) {
         let viz_render_data = tab.show(self.env, self.emu, ui);
         self.viz_render_data.extend(viz_render_data);
-    }
-
-    fn title(&mut self, tab: &mut Pane) -> egui::WidgetText {
-        tab.title().into()
     }
 }
